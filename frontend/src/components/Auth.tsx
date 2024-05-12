f4c670136b2a56029a8dc724abc22693fd3fd3b9
ansh4223 <ak210073@gmail.com>
2024-05-12 13:20:44 +0530

    feat:added frontend first page

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="text-3xl font-extrabold">Create an account</div>
+          <div className="text-slate-400">
+            Already have an account?
+            <Link className="pl-2 underline" to={"/sigin"}>Login</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+function LabelledInput({ label, placeholder, onChange}){
+    return <div>
+        <label for="first_Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+        <input type="text" id="first_name" class="bg-gray"
+    </div>
+}
